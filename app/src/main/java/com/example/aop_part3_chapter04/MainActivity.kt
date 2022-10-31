702af5dd6e32db34c3d76f19d58aa60740c1401a
Geon Kang <hottouk@naver.com>
2022-10-31 20:49:14 +0900

    검색 기능 구현 완료 그러나 검색어 저장 기능 Room 구현 오류 saveKeyword 해결할것

--- app/src/main/java/com/example/aop_part3_chapter04/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter04/MainActivity.kt
@@ -3,33 +3,57 @@ package com.example.aop_part3_chapter04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintSet.Motion
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.aop_part3_chapter04.adapter.BookAdapter
+import com.example.aop_part3_chapter04.adapter.HistoryAdapter
 import com.example.aop_part3_chapter04.api.BookService
 import com.example.aop_part3_chapter04.model.Book
 import com.example.aop_part3_chapter04.model.BookSearchDto
+import com.example.aop_part3_chapter04.model.History
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val bookRecyclerView: RecyclerView by lazy {
         findViewById(R.id.book_recycler_view)
     }
+    val historyRecyclerView: RecyclerView by lazy {
+        findViewById(R.id.history_recycler_view)
+    }
+    val searchEditTextView: EditText by lazy {
+        findViewById(R.id.search_edit_textview)
+    }
+
+    private lateinit var bookService: BookService
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "BookSearchHistoryDB"
+        ).build()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://openapi.naver.com")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val bookService = retrofit.create(BookService::class.java)
+        bookService = retrofit.create(BookService::class.java)
         bookService.getBooksByName(
-            "20gUNDqtYcE2fjtSb5OJ",
-            "oJ1lbJub00",
+            getString(R.string.naver_API_ID),
+            getString(R.string.naver_API_SECRETKEY),
             "꿈"
         ).enqueue(object : Callback<BookSearchDto> {
             override fun onResponse(
@@ -50,20 +74,110 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<BookSearchDto>, t: Throwable) { //실패
                 Log.e(TAG, t.toString())
-
             }
         })
+
+        searchEditTextView.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                search(searchEditTextView.text.toString())
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun search(keyword: String) {
+        bookService.getBooksByName(
+            getString(R.string.naver_API_ID),
+            getString(R.string.naver_API_SECRETKEY),
+            keyword
+        )
+            .enqueue(object : Callback<BookSearchDto> {
+                override fun onResponse(
+                    call: Call<BookSearchDto>,
+                    response: Response<BookSearchDto>
+                ) {
+                    saveSearchKeyword(keyword)
+                    hideHistoryRecyclerView()
+                    if (!response.isSuccessful) {
+                        return
+                    }
+                    response.body()?.let {
+                        attachBookAdapter(it.books)
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BookSearchDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                    hideHistoryRecyclerView()
+                }
+            })
     }
 
     private fun attachBookAdapter(books: List<Book>) {
         val adapter = BookAdapter(books)
         bookRecyclerView.adapter = adapter
         bookRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
 
+    private fun attachHistoryAdapter(keywordsHistory: List<History>) {
+        val adapter = HistoryAdapter(keywordsHistory, historyDeleteClickedListener = { keyword ->
+            deleteSearchKeyword(keyword)
+        })
+        historyRecyclerView.adapter = adapter
+        historyRecyclerView.layoutManager = LinearLayoutManager(this)
+        initSearchEditText()
+    }
+
+    private fun initSearchEditText() {
+        searchEditTextView.setOnKeyListener { view, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                search(searchEditTextView.text.toString())
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+        searchEditTextView.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                showHistoryRecyclerView()
+            }
+                return@setOnTouchListener false
+        }
+    }
+
+
+    private fun saveSearchKeyword(keyword: String) {
+        thread {
+            db.historyDao().insertHistory(History(null, keyword))
+        }.start()
+    }
+
+    private fun deleteSearchKeyword(keyword: String) {
+        thread {
+            db.historyDao().delete(keyword)
+            showHistoryRecyclerView()
+        }.start()
+    }
+
+
+    private fun showHistoryRecyclerView() {
+        Thread {
+            val keywordsHistory = db.historyDao().getAll().reversed()
+            attachHistoryAdapter(keywordsHistory)
+            runOnUiThread {
+                historyRecyclerView.isVisible = true
+            }
+        }.start()
+    }
+
+    private fun hideHistoryRecyclerView() {
+        historyRecyclerView.isVisible = false
     }
 
 
     companion object {
         const val TAG = "mainn"
+
     }
 }
--- app/src/main/java/com/example/aop_part3_chapter04/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/aop_part3_chapter04/adapter/BookAdapter.kt
@@ -1,11 +1,12 @@
 package com.example.aop_part3_chapter04.adapter
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.aop_part3_chapter04.R
 import com.example.aop_part3_chapter04.model.Book
 
@@ -14,13 +15,22 @@ class BookAdapter(
 ) : RecyclerView.Adapter<BookAdapter.BookItemViewHolder>() {
     inner class BookItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val bookTitleTextView: TextView
+        val bookDescriptionTextView : TextView
+        val coverImgView : ImageView
 
         init {
             bookTitleTextView = itemView.findViewById(R.id.title_textview)
+            bookDescriptionTextView = itemView.findViewById(R.id.description_textview)
+            coverImgView = itemView.findViewById(R.id.cover_imgview)
         }
 
         fun bindViews(book: Book) {
             bookTitleTextView.text = book.title
+            bookDescriptionTextView.text = book.description
+            Glide
+                .with(coverImgView.context)
+                .load(book.coverImgUri)
+                .into(coverImgView)
         }
     }
 
